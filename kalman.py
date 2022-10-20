91f354d610a4b1199c8ac3a98cc8fa115e4220e3
Andy <ayeh616@gmail.com>
2022-10-19 20:32:15 -0700

    maybe working kalman

--- kalman.py
+++ kalman.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import control
 
-m = 1
-b = 10
-k = 20
+m = 10
+b = 3
+k = 5
 F = 1
 
 A = np.array([[   0,    1],
@@ -15,11 +16,24 @@ D = np.array([[0]])
 
 time = np.arange(0, 10, 0.1)
 u = F*np.ones(np.shape(time))
-sigma = 0.1
-y = u + np.random.normal(0, sigma, np.shape(time))
+
+v = 0.5
+w = 0.01
+P = np.array([[1, 0],
+              [0, 1]])
+Q = np.array([[w**2, 0],
+              [0, w**2]])
+R = np.array([[v**2]])
+
+x = np.array([[1],
+              [0]])
+
+sys = control.ss(A, B, C, D)
+tout, yout = control.forced_response(sys, time, u, x)
+noise = np.random.normal(0, v, np.shape(time))
+youtnoise = yout + noise  # 2x100
 
 def predict(A, x, B, u, P, Q):
-    # print(np.shape(A), np.shape(x), np.shape(B), np.shape(u), np.shape(P), np.shape(Q))
     x = A@x + B@u  # 2x1
     P = A@P@A.T + Q  # 2x2
     return x, P
@@ -27,34 +41,26 @@ def predict(A, x, B, u, P, Q):
 def update(C, x, y, P, R):
     K = P@C.T@np.linalg.inv(C@P@C.T + R)  # 2x1
     x = x + K@(y - C@x)  # 2x1
-    P = (np.eye(2) - K@C)@P@(np.eye(2) - K@C).T + K@R@K.T  # 2x2
+    # P = (np.eye(2) - K@C)@P@(np.eye(2) - K@C).T + K@R@K.T  # 2x2
+    P = (np.eye(2) - K@C)@P  # 2x2
     return x, P
 
-x = np.array([[1], 
-              [0]])
-P = np.array([[0.1, 0],
-              [0, 0.1]])  # covariance matrix
-Q = np.array([[0.1, 0],
-              [0, 0.1]])  # process noise matrix
-R = np.array([[sigma]])  # measurement noise matrix
 xhat = x
-print(np.shape(xhat))
 for i in range(0, len(time)):
-    uu = np.array([[u[i]]])  # input
-    # print(uu)
-    x, P = predict(A, x, B, uu, P, Q)
-    yy = np.array([[y[i]]])  # measurement
-    x, P = update(C, x, yy, P, R)
+    ui = np.array([[u[i]]])  # input
+    yi = np.array([[youtnoise[i]]])  # measurement
+    x, P = predict(A, x, B, ui, P, Q)
+    x, P = update(C, x, yi, P, R)
     xhat = np.append(xhat, x, axis=1)
 
-xhat = xhat[:,1:]  # delete first temp entry
-print(np.shape(xhat))
-print(np.shape(y))
+xhat = xhat[:, 1:]  # delete first temp entry
+print(f'xhat: {np.shape(xhat)}')
 
 plt.figure(1)
-plt.plot(time, u, label='true')
-plt.plot(time, y, label='noise')
+plt.plot(time, u, label='u')
+plt.plot(time, yout, label='y')
+plt.plot(time, youtnoise, label='y+noise')
 plt.plot(time, xhat[0,:], label='x')
-# plt.plot(time, xhat[1,:], label='v')
+plt.plot(time, xhat[1,:], label='v')
 plt.legend()
 plt.show()
