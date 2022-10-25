8a7fbda005078464a2965799fef9983ad37ba7ea
Andy <ayeh616@gmail.com>
2022-10-24 19:24:57 -0700

    add piv

--- kalman.py
+++ kalman.py
@@ -2,11 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import control
 
+# tuning constants
 m = 10
 b = 3
 k = 5
 F = 1
 
+# state space
 A = np.array([[   0,    1],
               [-k/m, -b/m]])
 B = np.array([[  0],
@@ -14,23 +16,25 @@ B = np.array([[  0],
 C = np.array([[1, 0]])
 D = np.array([[0]])
 
+# reference
 time = np.arange(0, 10, 0.1)
-u = F*np.ones(np.shape(time))
+ref = F*np.ones(np.shape(time))
 
-v = 0.1
-w = 0.1
+# tuning matrix
 P = np.array([[1, 0],
-              [0, 1]])
+              [0, 1]])  # covariance matrix
+w = 0.1  # std dev process noise
 Q = np.array([[w**2, 0],
-              [0, w**2]])
-R = np.array([[v**2]])
-
+              [0, w**2]])  # covariance process noise
+v = 0.1  # std dev measurement noise
+R = np.array([[v**2]])  # covariance measurement noise
 x = np.array([[1],
-              [0]])
+              [0]])  # initial state
 
+# simulate response data
 sys = control.ss(A, B, C, D)
-tout, yout = control.forced_response(sys, time, u, x)
-noise = np.random.normal(0, v, np.shape(time))
+tout, yout = control.forced_response(sys, time, ref, x)
+noise = np.random.normal(0, v, np.shape(time))  # assume guassian noise
 youtnoise = yout + noise  # 1x100
 
 def predict(A, x, B, u, P, Q):
@@ -41,24 +45,25 @@ def predict(A, x, B, u, P, Q):
 def update(C, x, y, P, R):
     K = P@C.T@np.linalg.inv(C@P@C.T + R)  # 2x1
     x = x + K@(y - C@x)  # 2x1
-    # P = (np.eye(2) - K@C)@P@(np.eye(2) - K@C).T + K@R@K.T  # 2x2
-    P = (np.eye(2) - K@C)@P  # 2x2
+    P = (np.eye(len(P)) - K@C)@P  # 2x2
     return x, P
 
+# x estimate
 xhat = np.array([[],
                  []])
 for i in range(0, len(time)):
-    ui = np.array([[u[i]]])  # input
-    yi = np.array([[youtnoise[i]]])  # measurement
+    # single values
+    ui = np.array([[ref[i]]])  # reference
+    yi = np.array([[youtnoise[i]]])  # measurement + noise
     x, P = predict(A, x, B, ui, P, Q)
     x, P = update(C, x, yi, P, R)
+    # save values
     xhat = np.append(xhat, x, axis=1)
-
-# xhat = xhat[:, 1:]  # delete first temp entry 2x100
 print(f'xhat: {np.shape(xhat)}')
 
+# plotting
 plt.figure(1)
-plt.plot(time, u, label='r')
+plt.plot(time, ref, label='r')
 plt.plot(time, yout, label='y')
 plt.plot(time, youtnoise, label='y+noise')
 plt.plot(time, xhat[0,:], label='x')
--- piv.py
+++ piv.py
@@ -1,59 +1,71 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import control
 
-m = 1
+# tuning constants
+m = 10
 b = 10
 k = 20
 F = 1
 kp = 350 #350
 ki = 300 #300
 kd = 50 #50
+kv = 10
 
-# A = np.array([[   0,    1],
-#               [-k/m, -b/m]])
-# B = np.array([[  0],
-#               [1/m]])
-# C = np.array([[1, 0]])
-# D = np.array([[0]])
-
+# reference
 dt = 0.01
-time = np.arange(0, 3, dt)
+time = np.arange(0, 10, dt)
 print(len(time))
 ref = F*np.ones(np.shape(time))
 
-prev_error = 0
+# initialize constants/arrays
 prev_time = -dt
-r_arr = []
-e_arr = []
-u_arr = []
+prev_error = 0
+prev2_error = 0
+prev_u = 0
+prev_x = 0
+integral = 0
 x1_curr = 0
 x2_curr = 0
 y1_curr = 0
 y2_curr = 0
-prev_u = 0
-prev2_error = 0
+r_arr = []
+e_arr = []
+u_arr = []
 x1_arr = []
 x2_arr = []
 y1_arr = []
 y2_arr = []
 
 for i in range(0, len(time)):
+    # single values
     ri = ref[i]
     ti = time[i]
     
+    # # pid discrete
+    # dt = ti - prev_time
+    # error = ri - y1_curr
+    # u = (prev_u 
+    #      + (kp+ki*dt+kd/dt)*error 
+    #      + (-kp-2*kd/dt)*prev_error 
+    #      + kd/dt*prev2_error)
+    # prev2_error = prev_error
+    # prev_error = error
+    # prev_time = ti
+    # prev_u = u
+    
+    # piv
     dt = ti - prev_time
     error = ri - y1_curr
-    u = (prev_u 
-         + (kp+ki*dt+kd/dt)*error 
-         + (-kp-2*kd/dt)*prev_error 
-         + kd/dt*prev2_error)
-    prev2_error = prev_error
-    prev_error = error
+    vhat = (x1_curr - prev_x)/dt
+    u1 = kp*error - vhat
+    integral = integral + error*dt
+    u2 = ki * integral - kv*vhat
+    prev_x = x1_curr
     prev_time = ti
-    prev_u = u
-   
-    # u = ri
+    u = u2
+    
+    # model discrete mass spring damper
+    # u = ri  # test open loop
     a11, a12 =         1,           dt
     a21, a22 = -(dt*k)/m, 1 - (dt*b)/m
     b1 = 0
@@ -69,22 +81,22 @@ for i in range(0, len(time)):
     x1_curr = x1_next
     x2_curr = x2_next
     
-    # print(np.shape(x1_next), np.shape(x2_next), np.shape(y_curr))
+    # save values
+    r_arr = np.append(r_arr, ri)
+    e_arr = np.append(e_arr, error)
+    u_arr = np.append(u_arr, u)
     x1_arr= np.append(x1_arr, x1_curr)
     x2_arr= np.append(x2_arr, x2_curr)
     y1_arr = np.append(y1_arr, y1_curr)
     y2_arr = np.append(y2_arr, y2_curr)
-    
-    r_arr = np.append(r_arr, ri)
-    e_arr = np.append(e_arr, error)
-    u_arr = np.append(u_arr, u)
 
+# plotting
 plt.figure(1)
 plt.plot(time, r_arr, 'b', label='r')
 plt.plot(time, e_arr, 'r', label='e')
 # plt.plot(time, u_arr, 'y', label='u')
-plt.plot(time, x1_arr, label='x1')
-plt.plot(time, x2_arr, label='x2')
+# plt.plot(time, x1_arr, label='x1')
+# plt.plot(time, x2_arr, label='x2')
 plt.plot(time, y1_arr, '--', label='y1')
 plt.plot(time, y2_arr, '--', label='y2')
 plt.legend()
