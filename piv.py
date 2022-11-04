4be0412ea3e03ca4e4d551114fe4019273f14581
Andy <ayeh616@gmail.com>
2022-11-03 23:08:58 -0700

    function forms

--- piv.py
+++ piv.py
@@ -13,8 +13,8 @@ ki = 1000
 kd = 0
 kv = 100
 # pid 350 300 50
-# piv 100 10 10(ref vel=0)
-# piv 0 1000 100(kp=0)
+# piv 10 1 10(ref omega=0)
+# piv 0 1000 100(kp=0, ref omega = 4)
 ref_omega = 4
 
 # reference
@@ -22,41 +22,26 @@ dt = 0.01
 time = np.arange(0, 10, dt)
 ref = F*np.ones(np.shape(time))
 
-# initialize constants/arrays
-prev_time = -dt
-prev_error = 0
-prev2_error = 0
-prev_u = 0
-integral = 0
-# global r_arr, e_arr, u_arr
-r_arr = np.zeros(len(time))
-e_arr = np.zeros(len(time))
-u_arr = np.zeros(len(time))
-x_arr = np.zeros((2, len(time)))
-y_arr = np.zeros((2, len(time)))
-
-for i in range(0, len(time)):
-    # # pid discrete
-    # dt = time[i] - prev_time
-    # prev_time = time[i]
-    # error = ref[i] - y_arr[0, i]
-    # u = (prev_u 
-    #      + (kp+ki*dt+kd/dt)*error 
-    #      + (-kp-2*kd/dt)*prev_error 
-    #      + kd/dt*prev2_error)
-    # prev2_error = prev_error
-    # prev_error = error
-    # prev_u = u
-
-    # piv
-    dt = time[i] - prev_time
-    prev_time = time[i]
-    error = ref[i] - y_arr[0, i]
-    u1 = kp*error + ref_omega - y_arr[1, i]
-    integral = integral + u1*dt
-    u2 = ki*integral - kv*y_arr[1, i]
-    u = u2
+def pid_d(idx, dt, ref, mea):
+    global r_arr, e_arr, u_arr, prev_u, prev_error, prev2_error
+    error = ref - mea
+    u = (prev_u 
+         + (kp+ki*dt+kd/dt)*error 
+         + (-kp-2*kd/dt)*prev_error 
+         + kd/dt*prev2_error)
+    prev2_error = prev_error
+    prev_error = error
+    # prev_time = ti
+    prev_u = u
     
+    # save values
+    r_arr[idx] = ref
+    e_arr[idx] = error
+    u_arr[idx] = u
+    return u
+
+def model_d(idx, dt, u):
+    global x_arr, y_arr, k, b, m
     # model discrete mass spring damper
     # u = ref[i]  # check open loop
     u = np.array([[u]])
@@ -68,17 +53,46 @@ for i in range(0, len(time)):
                   [0, 1]])
     D = np.array([[0],
                   [0]])
-    x_arr[:, i+1:i+2] = A@x_arr[:, i:i+1] + B@u
-    y_arr[:, i+1:i+2] = C@x_arr[:, i:i+1] + D@u
-    
+    x_arr[:, idx+1:idx+2] = A@x_arr[:, idx:idx+1] + B@u
+    y_arr[:, idx:idx+1] = C@x_arr[:, idx:idx+1] + D@u
+    return y_arr[0, idx:idx+1], y_arr[1, idx:idx+1]
+
+def piv(idx, dt, ref_theta, ref_omega, mea_theta, mea_omega):
+    global integral, kp, ki, kv, r_arr, e_arr, u_arr
+    error = ref_theta - mea_theta
+    u1 = kp*error + ref_omega - mea_omega
+    integral = integral + u1*dt
+    u2 = ki*integral - kv*mea_omega
     # save values
-    r_arr[i] = ref[i]
-    e_arr[i] = error
-    u_arr[i] = u
+    r_arr[idx] = ref_theta
+    e_arr[idx] = error
+    u_arr[idx] = u2
+    return u2
+
+# initialize constants/arrays
+global prev_time, prev_error, prev2_error, prev_u, integral
+prev_time = -dt
+prev_error = 0
+prev2_error = 0
+prev_u = 0
+integral = 0
+global r_arr, e_arr, u_arr, x_arr, y_arr
+r_arr = np.zeros(len(time))
+e_arr = np.zeros(len(time))
+u_arr = np.zeros(len(time))
+x_arr = np.zeros((2, len(time)))
+y_arr = np.zeros((2, len(time)))
+
+yx = 0
+yv = 0
+for i in range(0, len(time)):
+    dt = time[i] - prev_time
+    prev_time = time[i]
+    # u = pid_d(i, dt, ref[i], yx)
+    u = piv(i, dt, ref[i], ref_omega, yx, yv)
+    yx, yv = model_d(i, dt, u)
 
 # plotting
-# print(np.shape(y_arr), np.shape(y_arr[:, 0]), np.shape(y_arr[0, 0]))
-# print(np.shape(y_arr[0,:]), y_arr[:, 0], y_arr[0, 0])
 plt.figure(1)
 plt.plot(time, r_arr, 'b', label='r')
 plt.plot(time, e_arr, 'r', label='e')
@@ -89,20 +103,3 @@ plt.plot(time, y_arr[0, :], '--', label='y1')
 plt.plot(time, y_arr[1, :], '--', label='y2')
 plt.legend()
 plt.show()
-
-# def pid_d(idx, dt, ref, mea):
-#     global r_arr, e_arr, u_arr
-#     error = ref - mea
-#     # print(ref[idx], y_arr[0, idx])
-#     u = (prev_u 
-#          + (kp+ki*dt+kd/dt)*error 
-#          + (-kp-2*kd/dt)*prev_error 
-#          + kd/dt*prev2_error)
-#     prev2_error = prev_error
-#     prev_error = error
-#     # prev_time = ti
-#     prev_u = u
-#     r_arr[idx] = ref
-#     e_arr[idx] = error
-#     u_arr[idx] = u
-#     return u
