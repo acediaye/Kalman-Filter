8cf1bf7dcb0b869c622f0ea9097fbd850d39ba61
Andy <ayeh616@gmail.com>
2022-11-06 19:50:20 -0800

    fixed A to F matrix. prediction still needs fixing

--- kalman.py
+++ kalman.py
@@ -7,6 +7,7 @@ m = 10
 b = 10
 k = 20
 F = 5
+dt = 0.1
 
 # state space
 A = np.array([[   0,    1],
@@ -17,14 +18,14 @@ C = np.array([[1, 0]])
 D = np.array([[0]])
 
 # reference
-dt = 0.1
 time = np.arange(0, 10, dt)
+print(np.shape(time))
 ref = F*np.ones(np.shape(time))
 
 # tuning matrix
 P = np.array([[1, 0],
               [0, 1]])  # covariance matrix
-w = 0.5  # std dev process noise
+w = 0.1  # std dev process noise
 Q = np.array([[w**2, 0],
               [0, w**2]])  # covariance process noise
 v = 0.5  # std dev measurement noise
@@ -37,12 +38,14 @@ sys = control.ss(A, B, C, D)
 tout, yout = control.forced_response(sys, time, ref, x)
 noise = np.random.normal(0, 0.1, np.shape(time))  # assume guassian noise, mu, sigma
 youtnoise = yout + noise  # 1x100
-    
+
 def predict(A, x, B, u, P, Q, dt):
-    x = x + dt*(A@x + B@u)  # 2x1
-    P = P + dt*(A@P + P@A.T + Q)  # 2x2
-    # x = A@x + B@u
-    # P = A@P@A.T + Q
+    # x = x + dt*(A@x + B@u)  # 2x1
+    # P = P + dt*(A@P + P@A.T + Q)  # 2x2
+    F = (np.eye(len(A)) + dt*A)
+    x = F@x + dt*B@u
+    P = F@P@F.T + Q  # 2x2
+    # P = P + dt*(A@P + P@A.T) + Q + dt*dt*A@P@A.T  # 2x2
     return x, P
 
 def update(C, x, y, P, R):
@@ -53,8 +56,9 @@ def update(C, x, y, P, R):
     return x, P
 
 # x estimate
-xhat = x
-for i in range(0, len(time)-1):
+xhat = np.array([[],
+                 []])
+for i in range(0, len(time)):
     # single values
     ui = np.array([[ref[i]]])  # reference
     yi = np.array([[youtnoise[i]]])  # measurement + noise
@@ -63,6 +67,7 @@ for i in range(0, len(time)-1):
     # save values
     xhat = np.append(xhat, x, axis=1)
 print(f'xhat: {np.shape(xhat)}')
+print(f'P: {P}')
 
 # plotting
 plt.figure(1)
@@ -70,6 +75,6 @@ plt.figure(1)
 plt.plot(time, yout, label='y')
 plt.plot(time, youtnoise, label='y+noise')
 plt.plot(time, xhat[0,:], label='x')
-plt.plot(time, xhat[1,:], label='v')
+# plt.plot(time, xhat[1,:], label='v')
 plt.legend()
 plt.show()
