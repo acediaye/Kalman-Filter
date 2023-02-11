8cce0f1d1693e0c467e52666d06a678177c0fdb4
Andy <ayeh616@gmail.com>
2023-02-11 09:01:05 -0800

    cleaned kalman, fix P?

--- kalman.py
+++ kalman.py
@@ -25,83 +25,47 @@ ref = F*np.ones(np.shape(time))
 
 # tuning matrix
 P0 = np.array([[1, 0],
-              [0, 1]])  # covariance matrix
+               [0, 1]])  # covariance matrix
 # 0 == good, 1 == bad
 w = 0.2  # std dev process noise
 Q = np.array([[w**2, 0],
               [0, w**2]])  # covariance process noise
 v = 1 - w  # std dev measurement noise
 R = np.array([[v**2]])  # covariance measurement noise
+x0 = np.array([[0],
+               [0]])  # initial state
 
 # simulate response data with noise
-x0 = np.array([[0],
-              [0]])  # initial state
 tout, yout = control.forced_response(sys, time, ref, x0)
 noise = np.random.normal(0, 0.1, np.shape(time))  # assume guassian noise: mu, sigma
 youtn = yout + noise  # 1x100
 
-def predict(A, x, B, u, P, Q, dt):
-    u = np.array([[u]])
-    F = np.eye(len(A)) + dt*A
-    x = F@x + dt*B@u
-    P = F@P@F.T + Q  # 2x2
-    return x, P
+# saving array
+x = np.zeros((2, len(time)))  # 2xn
+xdot = np.zeros((2, len(time)))
+P = np.zeros((len(time),2,2))  # n 2x2 array
+Pdot = np.zeros((len(time),2,2))
 
-def update(C, x, y, P, R):
-    y = np.array([[y]])
-    K = P@C.T@np.linalg.inv(C@P@C.T + R)  # 2x1
-    x = x + K@(y - C@x)  # 2x1
-    P = P - K@C@P  # 2x2
-    return x, P
+# init
+x[:,[0]] = x0
+P[[0],:,:] = P0
+for i in range(0, len(time)-1):
+    xdot[:,[i]] = A@x[:,[i]] + B@ref[i].reshape(1,1)
+    P[[i],:,:] = A@P[[i],:,:]@A.T + Q
+    K = P[[i],:,:]@C.T@np.linalg.pinv(C@P[[i],:,:]@C.T + R)
+    x[:,[i]] = x[:,[i]] + K@(youtn[i].reshape(1,1) - C@x[:,[i]])
+    P[[i],:,:] = P[[i],:,:] - K@C@P[[i],:,:]
+    # integrate
+    x[:,[i+1]] = x[:,[i]] + xdot[:,[i]]*dt
 
-mode = 'array'  # function # array
-if __name__ == '__main__':
-    if mode == 'function':
-        # x estimate
-        xhat = np.zeros((2, len(time)))
-        for i in range(0, len(time)):
-            ui = ref[i]  # reference
-            yi = youtn[i]  # measurement + noise
-            x0, P0 = predict(A, x0, B, ui, P0, Q, dt)
-            x0, P0 = update(C, x0, yi, P0, R)
-            # save values
-            xhat[:,i:i+1] = x0
-        print(f'xhat: {np.shape(xhat)}')
-        
-        # plotting
-        plt.figure(1)
-        plt.plot(time, yout, label='y')
-        plt.plot(time, youtn, label='y+noise')
-        plt.plot(time, xhat[0,:], linewidth=2, label='xhat')
-        plt.plot(time, xhat[1,:], linewidth=2, label='vhat')
-        plt.title('function method')
-        plt.legend()
-        plt.show()
-
-    elif mode == 'array':
-        x = np.zeros((2, len(time)))
-        x[:,[0]] = x0
-        xdot = np.zeros((2, len(time)))
-        P = np.zeros((len(time),2,2))  # many 2x2 array
-        P[[0],:,:] = P0
-        Pdot = np.zeros((len(time),2,2))
-        for i in range(0, len(time)-1):
-            # print(np.shape(xdot[:,i:i+1]), np.shape(x[:,i:i+1]), np.shape(ref[[i]]))
-            xdot[:,[i]] = A@x[:,[i]] + B@ref[i].reshape(1,1)
-            P[[i],:,:] = A@P[[i],:,:]@A.T + Q
-            K = P[[i],:,:]@C.T@np.linalg.pinv(C@P[[i],:,:]@C.T + R)
-            x[:,[i]] = x[:,[i]] + K@(youtn[i].reshape(1,1) - C@x[:,[i]])
-            P[[i],:,:] = P[[i],:,:] - K@C@P[[i],:,:]
-            # integrate
-            x[:,[i+1]] = x[:,[i]] + xdot[:,[i]]*dt
-        # print(np.shape(x), np.shape(xdot), np.shape(P), np.shape(Pdot))
-        
-        # plotting
-        plt.figure(1)
-        plt.plot(tout, yout, label='y')
-        plt.plot(tout, youtn, label='y+noise')
-        plt.plot(time, x[0,:], label='x1')
-        plt.plot(time, x[1,:], label='x2')
-        plt.title('array method')
-        plt.legend()
-        plt.show()
+# plotting
+plt.figure(1)
+plt.plot(tout, yout, label='y')
+plt.plot(tout, youtn, label='y+noise')
+plt.plot(time, x[0,:], label='x1')
+plt.plot(time, x[1,:], label='x2')
+plt.title('kalman filter')
+plt.xlabel('time')
+plt.ylabel('magnitude')
+plt.legend()
+plt.show()
